249d7448089d494be36b41648e72b418a539c2d8
Bint-Abdallah <tambedou.fanta@ugb.edu.sn>
2025-10-17 16:59:50 +0000

    redirect

--- src/main/java/org/example/appgedbackend/config/FrontendController.java
+++ src/main/java/org/example/appgedbackend/config/FrontendController.java
@@ -1,15 +1,12 @@
 package org.example.appgedbackend.config;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontendController {
 
-    // Redirige toutes les routes non API vers index.html
-    @RequestMapping(value = {
-            "/{path:[^\\.]*}",
-            "/**/{path:[^\\.]*}"
-    })
+    @GetMapping(value = "/**/{[path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
     }
